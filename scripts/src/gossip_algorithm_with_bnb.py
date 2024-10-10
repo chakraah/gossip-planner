18571726f9518a1d1689db3fbf5ac1b6b6b004f9
chakraah <110467940+chakraah@users.noreply.github.com>
2024-10-10 17:19:12 +0200

    Update gossip_algorithm_with_bnb.py

--- scripts/src/gossip_algorithm_with_bnb.py
+++ scripts/src/gossip_algorithm_with_bnb.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct  7 13:57:33 2024
-
-@author: Chakraa
-"""
-
 """
 Gossip Algorithm
 @H. Chakraa
@@ -16,21 +9,21 @@ def gossip_algorithm_with_bnb(scenario, max_iterations):
     # Get a random solution
     current_solution = scenario.generate_random_solution()
     
-    # Compute the initial cost
+    # Compute the current cost of the solution
     current_cost = scenario.compute_solution_cost(current_solution)
     
     # Lists to track costs over time
     improved_solution_costs = [current_cost]    # Costs when a better solution is found
-    gossip_iteration_costs = [current_cost]    # Costs after each gossip iteration
+    total_iteration_costs = [current_cost]     # Costs after each task exchange
     
     # Get the possible robot pairs that can exchange tasks
     robot_pairs = scenario.select_compatible_robots()
     
-    iteration = 1  # Track iterations for stopping criteria
-    iteration_s = 1  # Track iterations for solution equilibrium
-    equilibrium = 0
+    iteration_count  = 1  # Track iterations for stopping criteria
+    total_iterations = 1  # Track the total number of iterations
+    convergence_iteration = 0  # Iteration at which equilibrium is reached
         
-    while iteration < max_iterations:
+    while iteration_count < max_iterations:
         # Shuffle the robot pairs randomly
         np.random.shuffle(robot_pairs)
         
@@ -41,16 +34,13 @@ def gossip_algorithm_with_bnb(scenario, max_iterations):
                 
             # Compute the cost for the newly obtained solution
             new_cost = scenario.compute_solution_cost(new_solution)
-            gossip_iteration_costs.append(new_cost)
+            total_iteration_costs.append(new_cost)
                 
-            # If the new solution is worse or the same, increment the iteration count
-            if new_cost >= current_cost:
-                iteration += 1
-                iteration_s += 1
-            else:
+            if new_cost < current_cost:
+                
                 # If a better solution is found, reset the iteration count
-                iteration = 1
-                equilibrium = iteration_s
+                iteration_count = 1
+                convergence_iteration = total_iterations
                 
                 # Update to the better solution
                 current_solution = new_solution
@@ -63,5 +53,11 @@ def gossip_algorithm_with_bnb(scenario, max_iterations):
                 
                 # Track the improved solution's cost
                 improved_solution_costs.append(current_cost)
+                
+            else:
+                
+                iteration_count += 1
+                
+            total_iterations += 1
     
-    return current_solution, improved_solution_costs, equilibrium, gossip_iteration_costs
+    return current_solution, improved_solution_costs, total_iteration_costs, convergence_iteration
