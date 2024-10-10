e65609ebe4a4a33201d38cd67cd28e9d908ae353
chakraah <110467940+chakraah@users.noreply.github.com>
2024-10-10 17:20:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,15 +11,14 @@ from scripts.src.gossip_algorithm import gossip_algorithm
 
 def main():
     
-    # Generating an example with 4 robots, and 40 tasks
-    task_requirements, robot_capabilities, cost_matrix = random_scenario_generator(num_robots=4, num_tasks=30)
-    
+    # Generating a random example
+    task_requirements, robot_capabilities, cost_matrix = random_scenario_generator(10, 40, 30, 5)
     # Create a Scenario instance
     scenario = TaskPlanner(task_requirements, robot_capabilities, cost_matrix)
-    
+
     # Print number of robots, sites, and tasks
     print(f"Number of sites: {scenario.num_sites}")
-    print(f"Number of measurments: {scenario.num_measurements}")
+    print(f"Number of measurements: {scenario.num_measurements}")
     print(f"Number of robots: {scenario.num_robots}")
     print(f"Number of tasks: {scenario.num_tasks}\n")
     
@@ -28,19 +27,19 @@ def main():
     
     # Run the gossip algorithm
     start_time = time.time()
-    task_distribution, total_costs, stopping_criteria = gossip_algorithm(scenario, max_iterations)
+    task_distribution, optimised_costs, termination_criteria, convergence_point = gossip_algorithm(scenario, max_iterations)
     end_time = time.time()
-
+    
     # Plot the cost over iterations
     plt.figure(figsize=(12, 8))
     
     # Plot cost after improvement
-    plt.plot(total_costs, marker='o', linestyle='-', color='b', 
+    plt.plot(optimised_costs, marker='o', linestyle='-', color='b', 
          label='Cost after improvement', linewidth=2.5, markersize=8)
 
     # Plot cost after tasks exchange
-    plt.plot(stopping_criteria, marker='s', linestyle='--', color='r', 
-         label='Cost after tasks exchange', linewidth=2.5, markersize=8)
+    plt.plot(termination_criteria, marker='s', linestyle='--', color='r', 
+         label='Cost after task exchanges', linewidth=2.5, markersize=8)
 
     # Labels and title
     plt.xlabel('Iterations', fontsize=16)
@@ -56,8 +55,9 @@ def main():
     plt.show()
     
     # Output the results
-    print(f"Cost: {total_costs[-1]}")
+    print(f"Cost: {optimised_costs[-1]}")
     print(f"Execution Time: {end_time - start_time:.4f} seconds")
+    print(f"Number of iterations to reach an equilibrium: {convergence_point}")
     
 if __name__ == "__main__":
     main()
